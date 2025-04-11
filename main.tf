d8ac53345961088f04dfdb8dd97a819ee4657526
SINGH SNEH <25021804@mahindra.com>
2025-04-11 12:27:21 +0530

    adding token vailditiy

--- main.tf
+++ main.tf
@@ -36,5 +36,6 @@ resource "aws_cognito_user_pool" "user_pool" {
 resource "aws_cognito_user_pool_client" "client" {
   name = var.user_pool_client_name
   user_pool_id = aws_cognito_user_pool.user_pool.id
+  token_validity_units =var.token_validity_units
 }
 
